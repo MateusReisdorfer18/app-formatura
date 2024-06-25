5a4509ac08ff5a52f8a76234e95021648cfbd41c
Mateus <mateusreisdorfer18@gmail.com>
2024-06-25 19:33:26 -0300

    configs att

--- app-formatura_nest/src/app.module.ts
+++ app-formatura_nest/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { EstadoService } from './estado/estado.service';
+import { EstadoController } from './estado/estado.controller';
+import { EstadoModule } from './estado/estado.module';
+import { TipoServicoModule } from './tipo-servico/tipo-servico.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { TurmaModule } from './turma/turma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, EstadoModule, TipoServicoModule, UsuarioModule, TurmaModule],
+  controllers: [AppController, EstadoController],
+  providers: [AppService, EstadoService],
 })
 export class AppModule {}
